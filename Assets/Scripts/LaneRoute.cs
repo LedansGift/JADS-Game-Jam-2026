f262e31a89a2953eecba9eaf1ad76ecbabbff5e3
Winterbird <jongeak.choi@ucalgary.ca>
2026-06-17 06:01:22 -0600

    Fixing broken instance of the Scripts manually.

--- Assets/Scripts/LaneRoute.cs
+++ Assets/Scripts/LaneRoute.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 public class LaneRoute : MonoBehaviour
 {
     [SerializeField]
+    private Transform spawnPoint;
+
+    [SerializeField]
     private Transform[] routeWaypoints;
+
+    public Transform GetSpawnPoint()
+    {
+        return spawnPoint;
+    }
 
     public Transform[] GetRouteWaypoints()
     {
